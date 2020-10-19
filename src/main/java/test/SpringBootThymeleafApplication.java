015e79df21849e440103084ee7a15bb09416eea2
Mohamed Timor <mohamedtimorr@gmail.com>
2020-10-19 16:54:52 +0200

    SpringBootThymeleaf

---
+++ src/main/java/test/SpringBootThymeleafApplication.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
+@SpringBootApplication
+public class SpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafApplication.class, args);
+	}
+
+}
